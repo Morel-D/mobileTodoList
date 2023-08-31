64de269416e9386f6d158df9eb4ca78d83167210
Morel-D <morelDen@gmail.com>
2023-08-31 02:30:15 +0100

    create service file with class to fetch data

--- .client/lib/service/listService.dart
+++ .client/lib/service/listService.dart
@@ -1,1 +1,14 @@
+import '../model/listModel.dart';
+import 'package:http/http.dart' as http;
 
+class ListService {
+  final String base_url = "http://localhost:5000/List";
+  static var client = http.Client();
+
+  static Future<List?> fetchList() async {
+    var response = await client.get(Uri.parse(base_url));
+    if (response.statusCode == 200) {
+      return listModelFromJson(response.body);
+    }
+  }
+}
